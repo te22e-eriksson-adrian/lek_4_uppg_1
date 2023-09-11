15d5a4c97f6ba696384b78b6a36eba6bbb6542bb
te22e-eriksson-adrian <adrian.eriksson@elev.ga.ntig.se>
2023-09-11 09:10:30 +0200

    Fungerande divisionsprogram med typomvandling

--- src/App.java
+++ src/App.java
@@ -1,5 +1,18 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner tangentbord = new Scanner(System.in);
+        System.out.println("Divisions program:");
+        System.out.print("Ange tal 1: ");
+        int tal1 = tangentbord.nextInt();
+        System.out.print("Ange tal 2: ");
+        int tal2 = tangentbord.nextInt();
+        double kvot;
+        kvot = (double) tal1 / tal2;
+        System.out.println("Kvoten är: "+kvot);
+        int hel_kvot = (int) kvot;
+        System.out.println("Kvoten är: "+hel_kvot);
+        tangentbord.close();
     }
 }
